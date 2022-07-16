c64a7b7dd6c55aa7b2f3b722e778ca93f692419a
Jenny Duan <jenny.s.duan@gmail.com>
2022-07-16 09:12:25 -0700

    Added camera features

--- NailArtApp/CameraViewController.swift
+++ NailArtApp/CameraViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class CameraViewController: UIViewController { //class for the second tab
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    @IBAction func takeSelfie(_ sender: Any) {
+        imagePicker.sourceType = .camera
+       present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageDisplay.image = selectedImage
+        }
         
+        imagePicker.dismiss(animated: true, completion: nil)
         
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
 
 }
